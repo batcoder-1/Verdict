baa7f4ed9fa5b28a7ccd0285cb09a4993c13d702
Naman Dadhich <ndmajor2005@gmail.com>
2026-06-17 00:03:18 +0530

    refractor

--- backend/database.py
+++ backend/database.py
@@ -15,5 +15,4 @@ def create_db_and_tables():
 def get_session():
    with Session(engine) as session:
     yield session
-print(SQLModel.metadata.tables.keys())
 SessionDep=Annotated[Session,Depends(get_session)]
