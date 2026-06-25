645123cb654451790b5aa9f7e9cf08f7bc51a6d0
Naman Dadhich <ndmajor2005@gmail.com>
2026-06-25 23:27:35 +0530

    Setup application layout and routing

---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "1rem",
+        borderBottom: "1px solid #333",
+      }}
+    >
+      Verdict
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/router.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import MainLayout from "./layouts/MainLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardPage />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
