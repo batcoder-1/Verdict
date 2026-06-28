4b430f20abdafb7a1b201c8c4a9381f83240eb61
Naman Dadhich <ndmajor2005@gmail.com>
2026-06-28 13:17:18 +0530

    removed unused function from backend

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,15 @@
-from sqlmodel import SQLModel,Session,create_engine
-from typing import Annotated 
+from sqlmodel import SQLModel, Session, create_engine
+from typing import Annotated
 from fastapi import Depends
 from backend.models.leetcodeStats import leetcodeProfile
 from backend.models.users import User
 from backend.models.codeforcesStats import codeforcesProfile
 from backend.config import DATABASE_URL
 
-database_url=DATABASE_URL
-connect_args={"check_same_thread":False}
-engine=create_engine(database_url)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+engine = create_engine(DATABASE_URL)
 
 def get_session():
-   with Session(engine) as session:
-    yield session
-SessionDep=Annotated[Session,Depends(get_session)]
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
