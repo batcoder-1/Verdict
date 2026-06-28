420763b2c2153e2e914cb6e3124832f4c2019d73
Naman Dadhich <ndmajor2005@gmail.com>
2026-06-28 21:16:17 +0530

    Update client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8000/cp_analyzer",
+  baseURL: "https://verdict-uhvt.vercel.app",
   withCredentials: true,
 });
